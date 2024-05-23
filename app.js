776155ea95c0d30c3fc5dc8b1872e2ea93b74a8d
Yousef tarek elgohary <yousefelgohary455@gmail.com>
2024-05-23 17:06:31 +0300

    🧩 final commit

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 const app = express();
 app.use(express.json());
 import revenueRoutes from './routers/revenue.routes.js'
 import memberRoutes from './routers/member.routes.js'
 import trainerRoutes from './routers/trainer.routes.js'
 
+app.use(cors());
 app.use('/api/revenues', revenueRoutes);
 app.use('/api/trainers', trainerRoutes);
 app.use('/api/members', memberRoutes);
